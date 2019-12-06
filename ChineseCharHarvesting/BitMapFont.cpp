156f217b99f28787898ccbcf92a71256f9632119
shawn Jin <jinchenxiangdan@gmail.com>
2019-12-06 16:34:29 -0700

    converting Bitmap font
    TODO: convert configure to C++

---
+++ ChineseCharHarvesting/BitMapFont.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+ 
+class BitMapFont {
+
+
+    public: 
+
+
+        BitMapFont(string name_in, int size_in, string characters_in, double padding_in) {
+            name        = name_in;
+            size        = size_in;
+            chacacters  = characters_in;
+            padding     = padding_in;
+            // down round size and padding
+            size    = std::ceil(size);
+            padding = std::ceil(padding);
+            // create bitmap 
+            int bitmap[chacacters.length()]; 
+
+            
+        }
+
+    private:
+        // initial valuse 
+        string  name        = "Courier New";
+        int     size        = 32;
+        string  chacacters  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890''ì!\"£$%&/()=?^è+òàù,.-<\\|;:_>*@#[]{}";
+        double  padding     = 0.02 * size;
+};
+
