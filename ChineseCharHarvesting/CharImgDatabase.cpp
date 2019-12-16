308612def2d51356f2c8c1e4d6fdc4d2e662116b
jinchenxiangdan <jinchenxiangdan@gmail.com>
2019-12-16 15:28:38 -0700

    added file checker and convert more matlab code

--- ChineseCharHarvesting/CharImgDatabase.cpp
+++ ChineseCharHarvesting/CharImgDatabase.cpp
@@ -6,15 +6,22 @@
  */ 
 #include <sqlite3.h>
 #include <iostream>
+#include <fstream>
 
 
-class BitmapToDatabase() {
+class BitmapToDatabase {
 
 public:
-    int BitmapToDatabase(char *filePath);
+    BitmapToDatabase(char *filePath);
+    int update();
 
 private:
     sqlite3 *database;
+    int page_range[2] = {6, 95};
+    int max_char = 999;             // # bigger than chars per page
+    char *char_img_pattern = "page%02d-char%05d.png";
+
+
     std::string createTable = "CREATE TABLE if NOT EXISTS char_bitmaps "
             "(page NUMERIC, "
             "idx NUMERIC, "
@@ -22,15 +29,18 @@ private:
     std::string createIndex = "CREATE INDEX IF NOT EXISTS page_idx "
             "ON char_bitmaps (page, idx); ";
     std::string query =  "EXPLAIN QUERY PLAN "
-            "SELECT page,idx FROM char_bitmaps WHERE page = '6';"
+            "SELECT page,idx FROM char_bitmaps WHERE page = '6';";
 
-}
+};
 
 /**
  * CONTRIBUTER
  * filePath: the path of a database file 
  */
-int BitmapToDatabase::BitmapToDatabase(char *filePath) {
+BitmapToDatabase::BitmapToDatabase(char *filePath) {
+    char *db_file = "";            // default path 
+    char *char_dir = "";
+
     using namespace std;
     int exit = 0;
     char *errorMessage;
@@ -38,26 +48,63 @@ int BitmapToDatabase::BitmapToDatabase(char *filePath) {
     exit = sqlite3_open(filePath, &database);
     // check open status?
     // create table 
-    exit = sqlite3_exec(database, sql.c_str(), NULL, 0, &errorMessage);
+    exit = sqlite3_exec(database, createTable.c_str(), NULL, 0, &errorMessage);
     if (exit != SQLITE_OK) {
         cerr << "Error Create Table" << endl;
-        sqlite3_free(messaggeError); 
-        return -1;
+        sqlite3_free(errorMessage); 
+        // return -1;
     } else {
         std::cout << "Table created Successfully" << std::endl;
     }
     // create index 
-    exit = sqlite3_exec(database, sql.c_str(), NULL, 0, &errorMessage);
+    exit = sqlite3_exec(database, createIndex.c_str(), NULL, 0, &errorMessage);
     if (exit != SQLITE_OK) {
         cerr << "Error Create Index" << endl;
-        sqlite3_free(messaggeError); 
-        return -1;
+        sqlite3_free(errorMessage); 
+        // return -1;
     } else {
         std::cout << "Index created Successfully" << std::endl;
     }
-    // 
 
-
-    sqlite3_close(database);
+}
+/**
+ *  Check if a file is exist.  
+ **/
+inline bool fexists(const char *filename) {
+    using namespace std;
+    ifstream ifile(filename);
+    return ifile.good();
 }
 
+int BitmapToDatabase::update() {
+    char buffer[50];
+    for (int page = page_range[0]; page <= page_range[1]; page++) {
+        std::cout << "Page:" + page;
+        bool found_first_char = false;
+
+        for (int index = 1; index < max_char; i++) {
+            int n = sprintf(buffer, char_img_pattern, page, index);
+            if (fexists(buffer) && !found_first_char) {
+                found_first_char = true;
+            } else {
+                break;
+            }
+
+            if (!found_first_char) {
+                continue;
+            }
+            
+
+        }
+    }
+    return -1;
+}
+
+/**
+ * ANCHOR Shawn Jin
+ * 
+ * 
+ */
+int main() {
+
+}
